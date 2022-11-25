1b814d20ca34c2a5ec069bb7aef00f30147e1497
Abhinav052 <abhiabhinav535l@gmail.com>
2022-11-25 22:52:46 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function addColor1() {
+    document.body.style.backgroundColor = "aqua";
+}
+
+function addColor2() {
+    document.body.style.backgroundColor = "pink";
+}
+
+function addColor3() {
+    document.body.style.backgroundColor = "blue";
+}
+
+function addColor4() {
+    document.body.style.backgroundColor = "green";
+}
+
+function addColor5() {
+    document.body.style.backgroundColor = "grey";
+}
